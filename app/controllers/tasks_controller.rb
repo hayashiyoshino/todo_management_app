26d233b8fc6a5b344b3ee630cf1caab68f1df646
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-01 16:43:08 +0900

    write actions in tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,25 @@
 class TasksController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+
 end
