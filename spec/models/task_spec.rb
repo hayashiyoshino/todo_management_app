857b2657cab219cbebda631f613ee67fb5e54105
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-04 12:28:57 +0900

    add task model test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'get title' do
+    task = Task.new(title: 'hello', description: 'helloworld')
+    expect(task.title).to eq 'hello'
+  end
+
+  it 'get description' do
+    task = Task.new(title: 'hello', description: 'helloworld')
+    expect(task.description).to eq 'helloworld'
+  end
+
+  it 'is not be valid without title' do
+    task = Task.new(title: '', description: 'today')
+    expect(task).not_to be_valid
+  end
+
+  it 'is not valid without description' do
+    task = Task.new(title: 'today', description: '')
+    expect(task).not_to be_valid
+  end
+
 end
