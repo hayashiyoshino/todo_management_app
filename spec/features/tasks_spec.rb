d15cc3f4d4526d5563f2d98cc90e4ec474e4e99d
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-05 12:08:28 +0900

    add label and local: true opsion

---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Task管理' do
+  scenario "Taskの新規作成時にtitleが''だとエラーが表示される" do
+    visit tasks_path
+    click_link '新規作成'
+    fill_in 'Title', with: ''
+    expect {
+      click_button 'Create Task'
+    }.to change(Task, :count).by(0)
+    expect(page).to have_content "Title can't be blank"
+    end
+
+
+
+end
