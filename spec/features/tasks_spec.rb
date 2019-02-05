5b0ea62c38c10783f83b9f3e9865f9155bdd41ea
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-05 14:54:04 +0900

    add scenario create_task

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 feature 'Task管理' do
+
+  scenario "Taskを作成する" do
+    visit tasks_path
+    click_link '新規作成'
+    fill_in 'Title', with: 'hello'
+    fill_in 'Description', with: 'helloworld'
+    click_button 'Create Task'
+    expect(page).to have_content 'TODOを新規作成しました！'
+  end
   scenario "Taskの新規作成時にtitleが''だとエラーが表示される" do
     visit tasks_path
     click_link '新規作成'
