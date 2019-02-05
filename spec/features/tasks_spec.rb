1e8db99711e260e61169236ca20e4c12e15dc2eb
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-05 18:21:00 +0900

    add pry, launcy, and add scenario '作成日時の順番で並び替えができていること', and set config save_and_open_page

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 feature 'Task管理' do
+  scenario "作成日時の順番で並び替えができていること" do
+    Task.create(id: 1, title: 'hi', description: 'hi')
+    Task.create(id: 2, title: 'hi', description: 'hihi2', created_at: Time.current + 1.days)
+    Task.create(id: 3, title: 'hiii', description: 'hihi3', created_at: Time.current + 2.days)
+    Task.create(id: 4, title: 'hiiiii', description: 'hihi4', created_at: Time.current + 3.days)
+    visit tasks_path
+    task = all('.task_list')
+    task_0 = task[0]
+    expect(task_0).to have_content "2"
+    save_and_open_page
+  end
 
   scenario "Taskを作成する" do
     visit tasks_path
@@ -29,6 +40,15 @@ feature 'Task管理' do
     }.to change(Task, :count).by(0)
     expect(page).to have_content "Descriptionを入力してください"
   end
+
+  # scenario "作成日時の降順で並び替えができていること" do
+  #   visit tasks_path
+  #   click_link '新規作成'
+  #   fill_in 'Title', with: 'hello'
+  #   fill_in 'Description', with: 'helloworld!!'
+  #   click_button '登録する'
+  #   expect.(page).first('li.todo_list').to have_content "helloworld!!"
+  # end
 
 
 end
