430473090570248f443aacb6e7b9e982d8978de0
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-05 16:00:35 +0900

    change test message en to ja

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -7,7 +7,7 @@ feature 'Task管理' do
     click_link '新規作成'
     fill_in 'Title', with: 'hello'
     fill_in 'Description', with: 'helloworld'
-    click_button 'Create Task'
+    click_button '登録する'
     expect(page).to have_content 'TODOを新規作成しました！'
   end
   scenario "Taskの新規作成時にtitleが''だとエラーが表示される" do
@@ -15,9 +15,9 @@ feature 'Task管理' do
     click_link '新規作成'
     fill_in 'Title', with: ''
     expect {
-      click_button 'Create Task'
+      click_button '登録する'
     }.to change(Task, :count).by(0)
-    expect(page).to have_content "Title can't be blank"
+    expect(page).to have_content "Titleを入力してください"
   end
 
   scenario "Taskの新規作成時にdescriptionが''だとエラーが表示される" do
@@ -25,9 +25,9 @@ feature 'Task管理' do
     click_link '新規作成'
     fill_in 'Description', with: ''
     expect {
-      click_button 'Create Task'
+      click_button '登録する'
     }.to change(Task, :count).by(0)
-    expect(page).to have_content "Description can't be blank"
+    expect(page).to have_content "Descriptionを入力してください"
   end
 
 
