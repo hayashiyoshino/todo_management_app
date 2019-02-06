a03f706eadc8990b7cf0a3d2b88952e6cf60a4b4
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-06 18:26:16 +0900

    modfy typographical in  scenario '期限が近い順で並び替えができていること'

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -3,6 +3,25 @@ require 'rails_helper'
 
 
 feature 'Task管理' do
+
+  scenario "期限が近い順で並び替えができていること" do
+    taskmodel = Task.create(id: 1, title: '期限1', description: '期限１', deadline: Time.current + 5.days)
+    Task.create(id: 2, title: '期限2', description: '期限２', deadline: Time.current + 2.days)
+    Task.create(id: 3, title: '期限3', description: '期限３', deadline: Time.current + 6.days)
+    Task.create(id: 4, title: '期限4', description: '期限４', deadline: Time.current + 10.days)
+    visit tasks_path
+    click_on '期限近い順で並べ替え'
+    task = all('.task_list')
+    task_0 = task[0]
+    task_1 = task[1]
+    task_2 = task[2]
+    task_3 = task[3]
+    expect(task_0).to have_content "2"
+    expect(task_1).to have_content "1"
+    expect(task_2).to have_content "3"
+    expect(task_3).to have_content "4"
+  end
+
   scenario "作成日時の順番で並び替えができていること" do
     Task.create(id: 1, title: 'hi', description: 'hi', deadline: Time.current + 15.day)
     Task.create(id: 2, title: 'hi', description: 'hihi2', created_at: Time.current + 1.days, deadline: Time.current + 12.days)
@@ -12,26 +31,8 @@ feature 'Task管理' do
     task = all('.task_list')
     task_0 = task[0]
     expect(task_0).to have_content "4"
-    save_and_open_page
   end
 
-  scenario "期限が近い順で並び替えができていること" do
-    Task.create(id: 5, title: '期限1', description: '期限１', deadline: Time.current + 5.days)
-    Task.create(id: 6, title: '期限2', description: '期限２', deadline: Time.current + 2.days)
-    Task.create(id: 7, title: '期限3', description: '期限３', deadline: Time.current + 6.days)
-    Task.create(id: 8, title: '期限4', description: '期限４', deadline: Time.current + 10.days)
-    visit tasks_path
-    click_link '期限近い順で並べ替え'
-    task = all('.tasl_list')
-    task_0 = task[0]
-    task_1 = task[1]
-    tasl_2 = task[2]
-    task_3 = task[3]
-    expect(task_0).to have_content "2"
-    expect(task_1).to have_content "5"
-    expect(task_2).to have_content "3"
-    expect(task_3).to have_content "4"
-  end
 
   scenario "Taskを作成する" do
     visit tasks_path
