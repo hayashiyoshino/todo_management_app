7ffda515a48fd53c6a27c110e4864c49bc1d82b9
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-07 18:43:26 +0900

    add factory faker data

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :task do
-    title           'hello'
-    description     'helloworld'
+    sequence(:title)          { Faker::Name.name }
+    sequence(:description)    { Faker::Lorem.sentence }
+    # sequence(:deadline)       { Faker::Date.between(2019-02-07, 2019-05-07) }
+    sequence(:status)         { Faker::Number.between(0, 2)}
   end
 end
