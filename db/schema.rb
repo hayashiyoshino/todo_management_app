4f923c1f93fc6298a1e8e31c2a2cc010e63fec35
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-07 10:27:09 +0900

    change_column :tasks, :status, :integer, null: false, default: 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_094051) do
+ActiveRecord::Schema.define(version: 2019_02_07_012059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_02_06_094051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "deadline"
-    t.integer "status"
+    t.integer "status", default: 0, null: false
   end
 
 end
