2fe714d27de51a2164580ce347268d95fcae2349
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-08 22:45:33 +0900

    add user to spec

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     sequence(:title)          { Faker::Name.name }
     sequence(:description)    { Faker::Lorem.sentence }
     sequence(:status)         { Faker::Number.between(0, 2)}
+    sequence(:user_id)        { Faker::Number.between(1, 2)}
   end
 end
--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -5,10 +5,11 @@ require 'rails_helper'
 feature 'Task管理' do
 
   scenario "期限が近い順で並び替えができていること" do
-    taskmodel = Task.create(id: 1, title: '期限1', description: '期限１', deadline: Time.current + 5.days)
-    Task.create(id: 2, title: '期限2', description: '期限２', deadline: Time.current + 2.days)
-    Task.create(id: 3, title: '期限3', description: '期限３', deadline: Time.current + 6.days)
-    Task.create(id: 4, title: '期限4', description: '期限４', deadline: Time.current + 10.days)
+    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
+    taskmodel = Task.create(id: 1, title: '期限1', description: '期限１', deadline: Time.current + 5.days, user_id: 1)
+    Task.create(id: 2, title: '期限2', description: '期限２', deadline: Time.current + 2.days, user_id: 1)
+    Task.create(id: 3, title: '期限3', description: '期限３', deadline: Time.current + 6.days, user_id: 1)
+    Task.create(id: 4, title: '期限4', description: '期限４', deadline: Time.current + 10.days, user_id: 1)
     visit tasks_path
     click_on '期限近い順で並べ替え'
     task = all('.task_list')
@@ -23,10 +24,11 @@ feature 'Task管理' do
   end
 
   scenario "作成日時の順番で並び替えができていること" do
-    Task.create(id: 1, title: 'hi', description: 'hi', deadline: Time.current + 15.day)
-    Task.create(id: 2, title: 'hi', description: 'hihi2', created_at: Time.current + 1.days, deadline: Time.current + 12.days)
-    Task.create(id: 3, title: 'hiii', description: 'hihi3', created_at: Time.current + 2.days, deadline: Time.current + 16.days)
-    Task.create(id: 4, title: 'hiiiii', description: 'hihi4', created_at: Time.current + 3.days, deadline: Time.current + 110.days)
+    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
+    Task.create(id: 1, title: 'hi', description: 'hi', deadline: Time.current + 15.day, user_id: 1)
+    Task.create(id: 2, title: 'hi', description: 'hihi2', created_at: Time.current + 1.days, deadline: Time.current + 12.days, user_id: 1)
+    Task.create(id: 3, title: 'hiii', description: 'hihi3', created_at: Time.current + 2.days, deadline: Time.current + 16.days, user_id: 1)
+    Task.create(id: 4, title: 'hiiiii', description: 'hihi4', created_at: Time.current + 3.days, deadline: Time.current + 110.days, user_id: 1)
     visit tasks_path
     task = all('.task_list')
     task_0 = task[0]
@@ -35,6 +37,7 @@ feature 'Task管理' do
 
 
   scenario "Taskを作成する" do
+    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
     visit tasks_path
     click_link '新規作成'
     fill_in 'Title', with: 'hello'
@@ -43,6 +46,7 @@ feature 'Task管理' do
     expect(page).to have_content 'TODOを新規作成しました！'
   end
   scenario "Taskの新規作成時にtitleが''だとエラーが表示される" do
+    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
     visit tasks_path
     click_link '新規作成'
     fill_in 'Title', with: ''
@@ -53,6 +57,7 @@ feature 'Task管理' do
   end
 
   scenario "Taskの新規作成時にdescriptionが''だとエラーが表示される" do
+    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
     visit tasks_path
     click_link '新規作成'
     fill_in 'Description', with: ''
@@ -63,11 +68,12 @@ feature 'Task管理' do
   end
 
   scenario "検索した文字列と一致するタイトルを返す" do
-    task1 = Task.create(id: 1, title: "hello", description: "oo")
-    task2 = Task.create(id: 2, title: "hellorspec", description: "oo")
-    task3 = Task.create(id: 3, title: "helloruby", description: "oo")
-    task4 = Task.create(id: 4, title: "helloworld", description: "oo")
-    task5 = Task.create(id: 5, title: "hellorails", description: "oo")
+    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
+    task1 = Task.create(id: 1, title: "hello", description: "oo", user_id: 1)
+    task2 = Task.create(id: 2, title: "hellorspec", description: "oo", user_id: 1)
+    task3 = Task.create(id: 3, title: "helloruby", description: "oo", user_id: 1)
+    task4 = Task.create(id: 4, title: "helloworld", description: "oo", user_id: 1)
+    task5 = Task.create(id: 5, title: "hellorails", description: "oo", user_id: 1)
     visit tasks_path
     fill_in 'タイトルで検索', with: 'rspec'
     click_button '検索'
@@ -75,11 +81,12 @@ feature 'Task管理' do
   end
 
   scenario "優先順位で検索する" do
-    task1 = Task.create(id: 1, title: "hello", description: "oo", priority: 0)
-    task2 = Task.create(id: 2, title: "hellorspec", description: "oo", priority: 1)
-    task3 = Task.create(id: 3, title: "helloruby", description: "oo", priority: 2)
-    task4 = Task.create(id: 4, title: "helloworld", description: "oo", priority: 3)
-    task5 = Task.create(id: 5, title: "hellorails", description: "oo", priority: 0)
+    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
+    task1 = Task.create(id: 1, title: "hello", description: "oo", priority: 0, user_id: 1)
+    task2 = Task.create(id: 2, title: "hellorspec", description: "oo", priority: 1, user_id: 1)
+    task3 = Task.create(id: 3, title: "helloruby", description: "oo", priority: 2, user_id: 1)
+    task4 = Task.create(id: 4, title: "helloworld", description: "oo", priority: 3, user_id: 1)
+    task5 = Task.create(id: 5, title: "hellorails", description: "oo", priority: 0, user_id: 1)
     visit tasks_path
     select '緊急度３', from: 'Sortpriority'
     click_button '緊急度で検索'
