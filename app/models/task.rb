b3111419661bac53373ee8eb773b4a6f5576cd48
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-08 12:16:27 +0900

    move search controller to model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   enum status: [:未着手, :着手中, :完了]
+
+  def self.search(keyword = nil)
+    val ||= ""
+    where('title LIKE(?)', "%#{keyword}%")
+  end
+
 end
