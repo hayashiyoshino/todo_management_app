dc6f1ffa43cf4f8a7daf649ca6ff4d2c0aa00c0d
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-08 18:40:51 +0900

    add column options

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 2019_02_08_091630) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
