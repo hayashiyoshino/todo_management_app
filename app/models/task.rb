100275777f8ef6e445f47c8b028d69787ee067fd
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-08 16:51:04 +0900

    defined priority registration function

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   enum status: [:未着手, :着手中, :完了]
+  enum priority: [:緊急度０, :緊急度１, :緊急度２, :緊急度３]
 
   def self.search(keyword = nil)
     val ||= ""
