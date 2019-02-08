8457dfb9d19c2be469e7c9804fc818e1b17fde74
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-08 19:01:32 +0900

    add user_id column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_08_091630) do
+ActiveRecord::Schema.define(version: 2019_02_08_095429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_02_08_091630) do
     t.date "deadline"
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 0, null: false
+    t.integer "user_id"
     t.index ["title"], name: "index_tasks_on_title"
   end
 
