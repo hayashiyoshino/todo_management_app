7d4780d8045a3f708600100242c6798efa6d366b
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-09 22:40:58 +0900

    add validates email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks
   validates :name, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
 end
