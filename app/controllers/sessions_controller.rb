8e51c3d48dc4a72bec70d3bf75bf9111380f5f43
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-10 14:44:15 +0900

    defined login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user_path(session[:user_id])
+    else
+      flash[:notice] = 'invalid email/password combination'
+      render :new
+    end
+  end
+
+  def destroy
+  end
 end
