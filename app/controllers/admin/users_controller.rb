1562899ec9b33e72ef1b6c6683ac1c12b655a0f7
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-11 16:18:03 +0900

    add actions in admin/user_controller.rb

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,7 +5,54 @@ class Admin::UsersController < ApplicationController
     @users = User.all.order("created_at DESC")
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(prams[:id])
+    if @user = User.update(user_params)
+      redirect_to :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to :index
+      flash[:notice] = 'ユーザーを削除しました'
+    else
+      render :index
+      flash[:notice] = 'ユーザー削除に失敗しました'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+  end
+
   def admin_user
     redirect_to(root_path) unless current_user.admin?
   end
