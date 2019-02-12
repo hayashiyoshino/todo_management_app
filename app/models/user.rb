f24b32438060f4b42168edea734807246f38b074
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-12 17:15:56 +0900

    add collbacks check_admin_exist

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
   validates :password, presence: true, length: {minimum: 6}
+  before_destroy :check_admin_exist
+  before_update :check_admin_exist
+
+  def check_admin_exist
+     raise "管理者はこれ以上減らせません" if User.where(admin: true).size <= 1
+  end
 end
