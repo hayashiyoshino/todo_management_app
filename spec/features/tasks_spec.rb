6cf01281c4a0e3fbdc81fd032c75c5ee713c7282
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-19 08:25:55 +0900

    change features/tasks_spec.rb to do login

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -8,8 +8,8 @@ feature 'Task管理' do
 
   scenario "期限が近い順で並び替えができていること" do
     4.times { FactoryGirl.create(:task) }
-    visit tasks_path
-    fill_in 'Email', with: 'ttt@gmail.com'
+    visit '/login'
+    fill_in 'Email', with: 'tester1@example.com'
     fill_in 'Password digest', with: '11111111'
     click_on 'ログイン'
     click_on 'TODOリスト'
@@ -23,7 +23,11 @@ feature 'Task管理' do
 
   scenario "期限が遠い順で並び替えができていること" do
     4.times { FactoryGirl.create(:task) }
-    visit tasks_path
+    visit '/login'
+    fill_in 'Email', with: 'tester5@example.com'
+    fill_in 'Password digest', with: '11111111'
+    click_on 'ログイン'
+    click_on 'TODOリスト'
     click_on '期限遠い順で並べ替え'
     tasks = all('.task_item')
     expect(tasks[0]).to have_content "task8"
@@ -34,7 +38,11 @@ feature 'Task管理' do
 
   scenario "作成日時の順番で並び替えができていること" do
     4.times { FactoryGirl.create(:task) }
-    visit tasks_path
+    visit '/login'
+    fill_in 'Email', with: 'tester9@example.com'
+    fill_in 'Password digest', with: '11111111'
+    click_on 'ログイン'
+    click_on 'TODOリスト'
     tasks = all('.task_item')
     expect(tasks[0]).to have_content "9"
   end
@@ -50,35 +58,44 @@ feature 'Task管理' do
   # end
 
   scenario "Taskの新規作成時にtitleが''だとエラーが表示される" do
-    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
-    visit tasks_path
+    FactoryGirl.create(:user)
+    visit '/login'
+    fill_in 'Email', with: 'tester13@example.com'
+    fill_in 'Password digest', with: '11111111'
+    click_on 'ログイン'
+    click_on 'TODOリスト'
     click_link '新規作成'
     fill_in 'Title', with: ''
-    expect {
-      click_button '登録する'
-    }.to change(Task, :count).by(0)
+    fill_in 'Description', with: 'aaa'
+    click_button '登録する'
     expect(page).to have_content "Titleを入力してください"
   end
 
   scenario "Taskの新規作成時にdescriptionが''だとエラーが表示される" do
-    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
-    visit tasks_path
+    FactoryGirl.create(:user)
+    visit '/login'
+    fill_in 'Email', with: 'tester14@example.com'
+    fill_in 'Password digest', with: '11111111'
+    click_on 'ログイン'
+    click_on 'TODOリスト'
     click_link '新規作成'
+    fill_in 'Title', with: 'aaa'
     fill_in 'Description', with: ''
-    expect {
-      click_button '登録する'
-    }.to change(Task, :count).by(0)
+    click_button '登録する'
     expect(page).to have_content "Descriptionを入力してください"
   end
 
   scenario "検索した文字列と一致するタイトルを返す" do
-    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
-    Task.create(id: 1, title: "hello", description: "oo", user_id: 1)
-    Task.create(id: 2, title: "hellorspec", description: "oo", user_id: 1)
-    Task.create(id: 3, title: "helloruby", description: "oo", user_id: 1)
-    Task.create(id: 4, title: "helloworld", description: "oo", user_id: 1)
-    Task.create(id: 5, title: "hellorails", description: "oo", user_id: 1)
-    visit tasks_path
+    FactoryGirl.create(:task, title: "hello")
+    FactoryGirl.create(:task, title: "hellorspec")
+    FactoryGirl.create(:task, title: "helloruby")
+    FactoryGirl.create(:task, title: "helloworld")
+    FactoryGirl.create(:task, title: "hellorails")
+    visit '/login'
+    fill_in 'Email', with: 'tester15@example.com'
+    fill_in 'Password digest', with: '11111111'
+    click_on 'ログイン'
+    click_on 'TODOリスト'
     fill_in 'タイトルで検索', with: 'rspec'
     click_button '検索'
     expect(page).to have_content "hellorspec"
@@ -90,7 +107,11 @@ feature 'Task管理' do
     FactoryGirl.create(:task, title: "helloruby", status: 2)
     FactoryGirl.create(:task, title: "helloworld", status: 1)
     FactoryGirl.create(:task, title: "hellorails", status: 0)
-    visit tasks_path
+    visit '/login'
+    fill_in 'Email', with: 'tester20@example.com'
+    fill_in 'Password digest', with: '11111111'
+    click_on 'ログイン'
+    click_on 'TODOリスト'
     select '完了'
     click_button 'ステータスで検索'
     expect(page).to have_content "ruby"
@@ -100,13 +121,16 @@ feature 'Task管理' do
   end
 
   scenario "優先順位で検索する" do
-    user = User.create(id: 1, name: "hayashi", email: 'ttt@com')
-    Task.create(id: 1, title: "hello", description: "oo", priority: 0, user_id: 1)
-    Task.create(id: 2, title: "hellorspec", description: "oo", priority: 1, user_id: 1)
-    Task.create(id: 3, title: "helloruby", description: "oo", priority: 2, user_id: 1)
-    Task.create(id: 4, title: "helloworld", description: "oo", priority: 3, user_id: 1)
-    Task.create(id: 5, title: "hellorails", description: "oo", priority: 0, user_id: 1)
-    visit tasks_path
+    FactoryGirl.create(:task, title: "hello", priority: 0)
+    FactoryGirl.create(:task, title: "hellorspec", priority: 1)
+    FactoryGirl.create(:task, title: "helloruby", priority: 2)
+    FactoryGirl.create(:task, title: "helloworld", priority: 3)
+    FactoryGirl.create(:task, title: "hellorails", priority: 0)
+    visit '/login'
+    fill_in 'Email', with: 'tester25@example.com'
+    fill_in 'Password digest', with: '11111111'
+    click_on 'ログイン'
+    click_on 'TODOリスト'
     select '緊急度３'
     click_button '緊急度で検索'
     expect(page).to have_content "world"
