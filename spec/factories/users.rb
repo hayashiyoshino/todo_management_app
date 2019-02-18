520cb9a912e21acce49bd037e7abc7ae1bff9cdf
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-18 12:23:29 +0900

    add sequence to email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name "Hayashi"
-    email "tester@example.com"
+    sequence(:email) { |n| "tester#{n}@example.com" }
     password_digest "11111111"
   end
 end
