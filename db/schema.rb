85800194810b077720977d78d36a5dac42ab5757
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-19 18:11:05 +0900

    create groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_092201) do
+ActiveRecord::Schema.define(version: 2019_02_19_090640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lavels", force: :cascade do |t|
     t.string "lavel_name", null: false
