15ed8410d650a3a87f32452c227eafeddf95f810
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-20 08:32:41 +0900

    create group_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_115850) do
+ActiveRecord::Schema.define(version: 2019_02_19_233031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer "group_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "group_name", null: false
