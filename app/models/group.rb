7ea39295260df5aec515d5f58da0435a64fe0548
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-19 21:30:53 +0900

    change assosiation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
   has_many :tasks
-  has_many :users, through: :tasks
+  has_many :users, through: :group_user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  has_many :groups, through: :tasks
+  has_many :groups, through: :group_user
   has_secure_password
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
