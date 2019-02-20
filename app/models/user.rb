1080d77df316372766ce38fb712c2a3fcbedc6aa
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-20 18:02:50 +0900

    add spaces {}

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :tasks
   has_secure_password
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: {minimum: 6}
 end
