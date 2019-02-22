524542db489fe901db873d7511beaff65b5bb59a
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-22 18:19:48 +0900

    add has_one_attached :image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :group_users
   has_many :groups, through: :group_users, dependent: :destroy
+  has_one_attached :image
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
