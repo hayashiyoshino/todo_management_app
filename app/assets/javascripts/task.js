400cf3164c7432a58c0d2893667ef2f89da93b72
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-23 14:20:20 +0900

    add if document.referrer == 'http://localhost:3000/login'

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -8,15 +8,17 @@ $(document).on('turbolinks:load', function() {
        $('#task_lavels').tagit('createTag', tags[i])
      }
   }
-  if (gon.overtasks.length != 0　) {
-    new_tasks = gon.overtasks.map(x =>'『' + x + '』\n')
-    new_task = new_tasks.join(',')
-    window.alert('これらのタスクの期限が過ぎています！！！！\n' + new_task)
-  }
-  if (gon.neartasks.length != 0 ) {
-    console.log(gon.neartasks)
-    new_neartasks = gon.neartasks.map(x =>'『' +  x + '』\n')
-    new_neartask = new_neartasks.join(',')
-    window.alert('これらのタスクの期限が迫っています！！\n' + new_neartask)
+  if (document.referrer == "http://localhost:3000/login"){
+    if (gon.overtasks.length != 0　) {
+      new_tasks = gon.overtasks.map(x =>'『' + x + '』\n')
+      new_task = new_tasks.join(',')
+      window.alert('これらのタスクの期限が過ぎています！！！！\n' + new_task)
+    }
+    if (gon.neartasks.length != 0 ) {
+      console.log(gon.neartasks)
+      new_neartasks = gon.neartasks.map(x =>'『' +  x + '』\n')
+      new_neartask = new_neartasks.join(',')
+      window.alert('これらのタスクの期限が迫っています！！\n' + new_neartask)
+    }
   }
 })
