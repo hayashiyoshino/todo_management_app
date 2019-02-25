c793d9f5f37fc04052c51e6ee85ec82605ad4f79
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-25 17:04:44 +0900

    add routing chart

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tasks/calendar' => 'tasks#calendar'
+  get 'tasks/chart' => 'tasks#chart'
   resources :tasks do
     put :sort
   end
