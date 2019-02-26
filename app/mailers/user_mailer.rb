e3ea41eb0cc68ad6c5bbf886343d987a4865fd86
林佳志乃 <twinklestaryoshino@gmail.com>
2019-02-26 15:41:34 +0900

    create mail html

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,5 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'twinklestaryoshino@gmail.com'
 
   def welcome_email(user)
+    @user = user
+    @url = 'https://agile-citadel-24997.herokuapp.com/'
+    mail(to: @user.email, subject: 'the deadline for the task is coming.')
   end
 end
